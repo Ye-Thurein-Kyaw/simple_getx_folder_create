5553368614ccb76b063045bae740d4211a527ceb
Ye Thurein Kyaw <yethureinkyaw@globalwave.company>
2024-12-13 14:04:11 +0630

    feature/add_eample_file

--- example/lib/pages/example/controller/example_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-
-import '../model/example_model.dart';
-import '../provider/example_provider.dart';
-
-class ExampleController extends GetxController with StateMixin<ExampleModel> {
-  final ExampleProvider _exampleProvider = Get.find(); 
-}
